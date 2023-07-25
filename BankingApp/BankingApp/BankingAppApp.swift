6695d613469bdc9f6bd87fade198d16435fb7019
seydouxxx <risc@kakao.com>
2023-07-25 22:09:28 +0900

    init

---
+++ BankingApp/BankingApp/BankingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingAppApp.swift
+//  BankingApp
+//
+//  Created by Lee Hojun on 2023/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BankingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
